59f725be67424800158e11a03b6103c55c438ef0
lorenzo116 <66092891+lorenzo116@users.noreply.github.com>
2021-09-13 18:46:56 -0600

    Navegation drawer

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import { ButtonWebComponent } from "./components/button/button.component";
 import { LoginCardWebComponent } from "./components/logincard.component/logincard.component";
 import { SignInCardWebComponent } from "./components/signincard.components/signincard.component";
 import { HeaderWebComponent } from "./components/header/header.component";
+import { NavegationDrawerWebComponent } from "./components/navegation.drawer/navegation.drawer.component";
 import './../style/facturero.global.scss';
 console.log('Hola');
