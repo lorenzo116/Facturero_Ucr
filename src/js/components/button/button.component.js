da9fafd498704ff1e4543a52ee933588c53f0b3f
Braulio <bjrp2799@gmail.com>
2021-10-12 17:59:16 -0600

    Login y registro
    Incluye los textflieds y boton

--- src/js/components/button/button.component.js
+++ src/js/components/button/button.component.js
@@ -4,6 +4,7 @@ import css from './button.component.css';
 export class ButtonWebComponent extends HTMLElement {
     constructor(){
         super();
+        this._id = 0;
         const template = document.createElement('template');
         template.innerHTML =`<style>${css}</style>` + html;
         this.attachShadow({mode: 'open'});
@@ -11,9 +12,10 @@ export class ButtonWebComponent extends HTMLElement {
 
     }
     connectedCallback() {
+        
         this.shadowRoot.querySelector('button').innerHTML = this.getAttribute('text');
+        this.shadowRoot.querySelector("button").addEventListener("click", this);
         this.shadowRoot.querySelector('button').addEventListener('click', function (e) {
-
             let x = e.clientX - e.target.offsetLeft;
             let y = e.clientY - e.target.offsetTop;
         
@@ -26,6 +28,25 @@ export class ButtonWebComponent extends HTMLElement {
                 ripples.remove();
             }, 1000);
         });
+    }
+    get id() { 
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    handleEvent(e) {
+        if (e.type === "click") {
+            
+            const MessageEvent = new CustomEvent("message", {
+                detail: { 
+                    id: this._id
+                },
+                bubbles: true,
+                composed: true
+            });
+            this.dispatchEvent(MessageEvent);
+        }
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { ButtonWebComponent } from "./components/button/button.component";
-import { HeaderWebComponent } from "./components/header/header.component";
 import { TextfieldWebComponent } from "./components/textflied/textfield.component";
 import { LoginWebComponent } from "./components/login/login.component";
+import { RegistroWebComponent } from "./components/registro/registro.component";
 import './../style/facturero.global.scss';
 console.log('Hola');
