927ca3555b8676c046136101f843e6542839fad6
lorenzo116 <66092891+lorenzo116@users.noreply.github.com>
2021-10-18 18:41:37 -0600

    logo

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import { ButtonWebComponent } from "./components/button/button.component";
 import { TextfieldWebComponent } from "./components/textflied/textfield.component";
 import { LoginWebComponent } from "./components/login/login.component";
 import { RegistroWebComponent } from "./components/registro/registro.component";
+import { LogoWebComponent } from "./components/logo/logo.component";
 import './../style/facturero.global.scss';
 console.log('Hola');
