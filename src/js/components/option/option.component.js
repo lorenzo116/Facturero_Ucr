23ac176c2d17b42814a58aa1b155eb1a5f9247dc
Braulio <bjrp2799@gmail.com>
2022-02-19 12:07:14 -0600

    OptionWebComponent

---
+++ src/js/components/option/option.component.js
@@ -0,0 +1,14 @@
+import html from './option.component.html';
+import css from './option.component.css';
+export class OptionWebComponent extends HTMLElement {
+    constructor(){
+        super();
+        const template = document.createElement('template');
+        template.innerHTML =`<style>${css}</style>` + html;
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+    connectedCallback() {
+    }
+}
+customElements.define('fa-option', OptionWebComponent);
