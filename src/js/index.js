9315cbe19f9980df5e96cfdcef8a2b4e2efa47f9
Braulio <bjrp2799@gmail.com>
2022-02-19 12:12:48 -0600

    cambios varios

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import { TextfieldWebComponent } from "./components/textflied/textfield.componen
 import { EmergencyloginWebComponent } from "./modules/login/emergencylogin.component";
 import { RegistroWebComponent } from "./components/registro/registro.component";import { LogoWebComponent } from "./components/logo/logo.component";
 import { MainlandingviewWebComponent } from "./modules/mainlandingview/mainlandingview.component";
-import { CreateaccountviewWebComponent } from "./modules/createaccountview/createaccountview.component";
+/*import { CreateaccountviewWebComponent } from "./modules/createaccountview/createaccountview.component";*/
 import { DropdownWebComponent } from "./components/dropdown/dropdown.component";
 import { ResultbillcreationWebComponent } from "./modules/resultbillcreation/resultbillcreation.component";
 import { LabelWebComponent } from "./components/label/label.component";
@@ -12,6 +12,18 @@ import { CalendarWebComponent } from "./components/caledar/calendar.component";
 import { CreatereservationWebComponent } from "./modules/createreservation/createreservation.component";
 import { TextareaWebComponent } from "./components/textarea/textarea.component";
 import { DrawerWebComponent } from "./components/drawer/drawer.component";
+import { SelectWebComponent } from "./components/select/select.component";
+import { OptionWebComponent } from "./components/option/option.component";
+import { PersontaxpayeraddWebComponent } from "./modules/persontaxpayeradd/persontaxpayeradd.component";
+import { UpdateusersectionWebComponent } from "./modules/updateusersection/updateusersection.component";
+import { SuccessregistrationviewWebComponent } from "./modules/successregistrationview/successregistrationview.component";
+import { TitleWebComponent } from "./components/title/title.component";
+import { ReportingviewWebComponent } from "./modules/reportingview/reportingview.component";
+import { LayoutWebComponent } from "./components/layout/layout.component";
+/**
+ *  *Este module se usa para pruebas
+ */
+import { PruebaWebComponent } from "./modules/prueba/prueba.component";
 
 import "side-drawer";
 import './../style/facturero.global.scss';
