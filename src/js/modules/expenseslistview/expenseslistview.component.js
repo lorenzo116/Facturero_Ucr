b0195181356859b6e553af4c6aa28bbdf037b8b4
Braulio <bjrp2799@gmail.com>
2022-02-21 19:47:11 -0600

    Revert "Revert "Merge branch 'Jimmy' into develop""
    This reverts commit cb3b539e32a8df51dec5615c83b391629cc95638.

--- src/js/modules/expenseslistview/expenseslistview.component.js
+++ src/js/modules/expenseslistview/expenseslistview.component.js
@@ -1,7 +1,7 @@
-import html from './loading.component.html';
-import css from './loading.component.css';
+import html from './expenseslistview.component.html';
+import css from './expenseslistview.component.css';
 
-export class loadingWebComponent extends HTMLElement {
+export class expenseslistviewComponent extends HTMLElement {
     constructor(){
         super();
         const template = document.createElement('template');
@@ -12,4 +12,4 @@ export class loadingWebComponent extends HTMLElement {
     connectedCallback() {
     }
 }
-customElements.define('fa-loading', loadingWebComponent);
+customElements.define('fa-expenseslistview', expenseslistviewComponent);
